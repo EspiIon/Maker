7043d4739f280ac16daddffe9dded62858e47058
Loïc <57635864+EspiIon@users.noreply.github.com>
2024-09-26 21:50:13 +0200

    ''

--- main.pas
+++ main.pas
@@ -1,36 +1,66 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import heapq
-import math
-# Paramètres de discrétisation
-h = 1  # pas de discrétisation
-n, m = 100, 100  # taille de la grille
-T = np.full((n, m), np.inf)  # fonction temps de passage (initialisée à l''infini)
-F = 5  # vitesse constante (peut être ajustée selon l''image)
+program main;
 
-# Point de départ du front (milieu de la grille)
-start_x, start_y = 40,30
-T[40,30],T[41,30],T[39,30],T[30,29],T[40,31]= 0,0,0,0,0  # temps de passage initial
+uses SDL2, SDL2_image;
 
-# File de priorité (tas) pour la méthode Fast Marching
-heap = [(0, start_x, start_y)]
+var sdlWindow1:PSDL_Window;
+    sdlRenderer:PSDL_Renderer;
+    surfaceMario,surfaceBackground:PSDL_Surface;
+    mario,background:PSDL_Texture;
+    destRectMario,destRectBackground:TSDL_Rect;
+    event:TSDL_Event;
+    quit:boolean;
+begin
+    if SDL_Init(SDL_INIT_VIDEO) < 0 then 
+        Halt;
 
-# Algorithme Fast Marching
-while heap:
-    t, x, y = heapq.heappop(heap)
-    
-    # Mise à jour des voisins de (x, y)
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < m:
-            # Calcul de la nouvelle valeur de T pour (nx, ny)
-            new_T = sqrt(h/F)
-            if new_T < T[nx, ny]:
-                T[nx, ny] = new_T
-                heapq.heappush(heap, (new_T, nx, ny))
+    sdlWindow1 := SDL_CreateWindow('window1',50,50,800,500, SDL_WINDOW_SHOWN);
 
-# Visualisation des courbes de niveau du front
-plt.figure(figsize=(6, 6))
-plt.contour(T, levels=20)
-plt.title('Propagation du front - Méthode Fast Marching')
-plt.show()
+    sdlRenderer := SDL_CreateRenderer(sdlWindow1, -1, 0);
+    surfaceMario := IMG_Load('./assets/mario.png');
+    surfaceBackground:= IMG_Load('./assets/background.png');
+    mario:= SDL_CreateTextureFromSurface(sdlRenderer,surfaceMario);
+    background:= SDL_CreateTextureFromSurface(sdlRenderer,surfaceBackground);
+    //Mario
+    destRectMario.x:=10;
+    destRectMario.y:=400;
+    destRectMario.w:=50;
+    destRectMario.h:=50;
+
+    //Background
+    destRectBackground.x:=0;
+    destRectBackground.y:=-45;
+    destRectBackground.w:=900;
+    destRectBackground.h:=531;
+
+    quit := false;
+    while not quit do
+    begin
+        while SDL_PollEvent(@event) <> 0 do
+        begin
+
+            if event.type_ = SDL_QUITEV then
+            quit := true;
+
+            if event.type_ = SDL_KEYDOWN then
+            begin
+                case event.key.keysym.sym of 
+                    SDLK_ESCAPE:quit:=True;
+                    SDLk_right:destRectMario.x:= destRectMario.x +5;
+                    SDLk_left:destRectMario.x:= destRectMario.x -5;
+                end;
+            end;
+
+
+            SDL_RenderClear(sdlRenderer);
+            SDL_RenderCopy(sdlRenderer,background,nil, @destRectBackground);
+            SDL_RenderCopy(sdlrenderer,mario,nil,@destRectMario);
+
+            SDL_RenderPresent(sdlRenderer);
+        end;
+    end;
+
+    SDL_DestroyRenderer(sdlrenderer);
+    SDL_DestroyWindow(sdlWindow1);
+    SDL_Quit;
+
+end.
