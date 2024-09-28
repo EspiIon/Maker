673cb23e87195965fb7a00c55e966a82298797e3
Loïc <57635864+EspiIon@users.noreply.github.com>
2024-09-28 20:20:58 +0200

    tableau background

--- main.pas
+++ main.pas
@@ -1,74 +1,145 @@
-
 program main;
 
 uses SDL2, SDL2_image;
 
+const absoluteTop = 250;
+const taille = 3;
 
-// Define records for Tbackground and Tplayer types
-type Tbackground = record
-  destRect:TSDL_Rect;
+type Tbackground =
+    record
+    destRect:TSDL_Rect;
     surface:PSDL_Surface;
     texture:PSDL_Texture;
-end;
-type Tplayer = record
+    end;
+type Tplayer =
+    record
     destRect:TSDL_Rect;
     life:integer;
     surface:PSDL_Surface;
     texture:PSDL_Texture;
-end;
+    end;
+type TabBackground = array[1..taille] of Tbackground;
+var
+    FloorLevel,top,i:integer;
+    counter:real;
+    sdlWindow1:PSDL_Window;
+    sdlRenderer:PSDL_Renderer;
+    player:Tplayer;
+    background:TabBackground;
+    left,right,up,down:boolean;
+    event:TSDL_Event;
+    quit:boolean;
 
-// Define a procedure to move the player and background
-procedure move(var player:Tplayer;var background:Tbackground;up,right,left:boolean);
-begin
-    // Move the player up if 'up' is true
-    if (up = True)then
+
+procedure GoodPosition(var player:Tplayer;var background:TabBackground);
+var i:integer;
+    begin
+    if player.destRect.x < 0 then
+        player.destRect.x:=0;
+    if player.destRect.x > 400 then
+        player.destRect.x:=400;
+
+    for i:=1 to taille do
         begin
-            player.destRect.y:= player.destRect.y -6;
+        if background[i].destRect.x < -1800 then
+            begin
+            background[i].destRect.x:=895*(taille-1);
+            end;
+        if background[i].destRect.x >1800 then
+            begin
+            background[i].destRect.x:=-895-taille;
+            end;
         end;
+    end;
 
-    // Move the background left if 'right' is true (this is a mistake, it should be 'left')
+procedure move(var player:Tplayer;var background:TabBackground;up,right,left:boolean;top:integer;var counter:real);
+var speed,i:integer;
+begin
+    
+    if (up = True)then
+    begin
+        speed:=round((player.destRect.y - top)/30)+2;
+        player.destRect.y:= player.destRect.y -speed;
+    end;
     if (right = True)then
         begin
-            background.destRect.x:= background.destRect.x -7;
-        end;
-
-    // Move the background right if 'left' is true (this is a mistake, it should be 'right')
-    if (left = True)then
+        if player.destRect.x=400 then
         begin
-            background.destRect.x:= background.destRect.x +7;
+        for i:=1 to taille do
+            background[i].destRect.x:= background[i].destRect.x + round(counter);
+        end
+        else
+            player.destRect.x:=player.destRect.x-round(counter);
+        if counter > -7 then
+            counter:= counter-0.2;
+        end;
+    if (left = True)then
+    begin
+        if player.destRect.x = 0 then
+        begin
+        for i:=1 to taille do
+            background[i].destRect.x:= background[i].destRect.x + round(counter);
+        end
+        else
+            player.destRect.x:=player.destRect.x-round(counter);
+    if counter < 7 then
+        counter:= counter+0.2;
+    end;
+end;
+
+procedure Gravity(var player:Tplayer;var up,down:boolean;level:integer;var top:integer);
+var speed:integer;
+begin
+    if (player.destRect.y = top) or (player.destRect.y < absoluteTop)  then
+    begin
+        down:=True;
+        up:=False;
+    end;
+    if (player.destRect.y >= level) then
+        begin
+        down:=False;
+        top:=absoluteTop;
+        if player.destRect.y <> FloorLevel then
+        player.destRect.y := FloorLevel;
+        end;
+    if down then
+        begin
+        speed:=round((player.destRect.y -top)/30)+2;
+        player.destRect.y:= player.destRect.y + speed;
         end;
 end;
 
 
-begin
-    // Initialize SDL and create a window and renderer
 
+begin
+    FloorLevel:=400;
+    top:=absoluteTop;
     if SDL_Init(SDL_INIT_VIDEO) < 0 then
         Halt;
 
-    sdlWindow1 := SDL_CreateWindow('window1',50,50,800,500, SDL_WINDOW_SHOWN);
+    sdlWindow1 := SDL_CreateWindow('window1',50,50,1280,720, SDL_WINDOW_SHOWN);
     sdlRenderer := SDL_CreateRenderer(sdlWindow1, -1, 0);
 
-    // Load player and background surfaces and create textures from them
     player.surface := IMG_Load('./assets/mario.png');
-    background.surface:= IMG_Load('./assets/background.png');
+    for i:=1 to taille do
+        begin
+        background[i].surface:= IMG_Load('./assets/background.png');
+        background[i].texture:= SDL_CreateTextureFromSurface(sdlRenderer,background[i].surface);
+        background[i].destRect.x:=0+900*(i-1);
+        background[i].destRect.y:=0;
+        background[i].destRect.w:=900;
+        background[i].destRect.h:=500;
+        end;
     player.texture:= SDL_CreateTextureFromSurface(sdlRenderer,player.surface);
-    background.texture:= SDL_CreateTextureFromSurface(sdlRenderer,background.surface);
 
-    // Set initial positions and dimensions for the player and background
-    player.destRect.x:=400;
+
+    //Mario
+    player.destRect.x:=20;
     player.destRect.y:=400;
     player.destRect.w:=50;
     player.destRect.h:=50;
 
-    background.destRect.x:=0;
-    background.destRect.y:=-45;
-    background.destRect.w:=900;
-    background.destRect.h:=531;
-
     quit := false;
-
-    // Main game loop
     while not quit do
     begin
 
@@ -109,7 +180,7 @@ begin
             //rendement
             
             SDL_RenderClear(sdlRenderer);
-            for i:=1 to 2 do
+            for i:=1 to taille do
                 begin
                 SDL_RenderCopy(sdlRenderer,background[i].texture,nil, @background[i].destRect);
                 end;
@@ -136,7 +207,7 @@ begin
         end;
 
         SDL_RenderClear(sdlRenderer);
-        for i:=1 to 2 do
+        for i:=1 to taille do
             begin
             SDL_RenderCopy(sdlRenderer,background[i].texture,nil, @background[i].destRect);
             end;
